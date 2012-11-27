d2cde78195cdabc64a6b10b29b3ce999777ad7ab
Izhar Firdaus <izhar@inigo-tech.com>
2012-11-27 23:30:23 +0800

    initial import (re #88)

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='wcc.vocabularies',
+      version=version,
+      description="Vocabularies for WCC",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Inigo Consulting',
+      author_email='team@inigo-tech.com',
+      url='http://github.com/inigoconsulting/wcc.vocabularies',
+      license='gpl',
+      packages=find_packages(),
+      namespace_packages=['wcc'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.schema',
+          'zope.interface',
+          'zope.component',
+          'z3c.formwidget',
+          'five.grok'
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
