ea71f3798846d2ea434753028f1b03eda4cfa34d
Izhar Firdaus <izhar@inigo-tech.com>
2012-11-28 22:30:57 +0800

    add country vocabulary

--- wcc/vocabularies/countries.py
+++ wcc/vocabularies/countries.py
@@ -4,12 +4,14 @@ from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.schema.interfaces import IVocabularyFactory
 from zope.component import getUtility
 from z3c.formwidget.query.interfaces import IQuerySource
+import pycountry
 
-VALUES=[]
 
 class VocabularyFactory(object):
     def __call__(self, context):
-        return SimpleVocabulary.fromValues(sorted(VALUES))
+        terms = [SimpleTerm(value=c.alpha2, 
+                    title=c.name) for c in pycountry.countries]
+        return SimpleVocabulary(terms)
 
 grok.global_utility(VocabularyFactory, IVocabularyFactory,
         name='wcc.vocabulary.country')
